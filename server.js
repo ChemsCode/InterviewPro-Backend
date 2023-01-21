efbc55853aa5a0d2fced10d5f586e4ac68ce460b
ChemsCode <saidiceb@gmail.com>
2023-01-21 18:49:58 -0500

    fixed bug

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const cookieParser = require('cookie-parser');
-const errorHandler = require('./middleware/errorHandler');
-const credentials = require('./middleware/credentials');
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/errorHandler");
+const credentials = require("./middleware/credentials");
 const PORT = process.env.PORT || 3500;
-
 
 app.use(credentials);
 app.use(cors(corsOptions));
@@ -17,21 +16,23 @@ app.use(express.json());
 app.use(cookieParser());
 
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 // routes
-app.use('/', require('./routes/root'));
-app.use('/openai', require('./routes/api/openai'));
+app.use("/", require("./routes/root"));
+app.use("/openai", require("./routes/api/openai"));
 
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
